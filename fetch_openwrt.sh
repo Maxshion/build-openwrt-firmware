520f2b957b6fda1d09b3d6696335a16c05802841
Maxshion <32436708+Maxshion@users.noreply.github.com>
2021-11-20 01:38:50 +0800

    Update fetch_openwrt.sh

--- fetch_openwrt.sh
+++ fetch_openwrt.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-(git clone -b openwrt-21.02 --single-branch https://github.com/immortalwrt/immortalwrt openwrt) || (cd openwrt && git stash && git pull)
+(git clone -b openwrt-21.02 --single-branch https://github.com/coolsnowwolf/lede openwrt) || (cd openwrt && git stash && git pull)
 
 cd openwrt
 
