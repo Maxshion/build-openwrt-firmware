352662e394651a499eba3beaf073abe5730eedab
Maxshion <32436708+Maxshion@users.noreply.github.com>
2021-11-21 16:46:45 +0800

    Update pack_firmware.sh

--- pack_firmware.sh
+++ pack_firmware.sh
@@ -3,7 +3,7 @@
 OPENWRT_ARMVIRT="$(pwd)/bin/tmp/openwrt-armvirt-64-default-rootfs.tar.gz"
 PACKAGE_SOC="s905d"
 KERNEL_VERSION_NAME="5.4.160"
-OPENWRT_VER="immortal21.02"
-WHOAMI="river"
+OPENWRT_VER="LEDE"
+WHOAMI="Garrier"
 
 source openwrt_flippy.sh
